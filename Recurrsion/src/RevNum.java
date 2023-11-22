644b36ae889691bea4c4380069dbc9c42e8c81c2
Muskan-1003 <Muskanpandey1003@gmail.com>
2023-11-23 00:14:13 +0530

    java

--- Recurrsion/src/RevNum.java
+++ Recurrsion/src/RevNum.java
@@ -1,13 +1,22 @@
 public class RevNum {
     public static void main(String[] args) {
-
+        System.out.println(rev2(1234));
     }
     //first way
-    static int sum=0;
-    static void sum(int n){
-        if(n==0){
-            return;
+//    static int sum=0;
+
+    static int rev2(int n){
+        int digits=(int)(Math.log10(n))+1;
+        return helper(n,digits);
+    }
+    static int helper(int n, int digits){
+        if(n%10==n){
+            return n;
         }
+        int rem=n%10;
+        return rem *(int) Math.pow(10,digits-1)+helper(n/10,digits-1);
+
+
 
 
 
