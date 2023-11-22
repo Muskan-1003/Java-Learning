b513b81daa80c347592ab08ba59e35cc2ea7fe96
Muskan-1003 <Muskanpandey1003@gmail.com>
2023-11-22 23:56:46 +0530

    java

--- Recurrsion/src/FibbonaciNo.java
+++ Recurrsion/src/FibbonaciNo.java
@@ -1,6 +1,6 @@
 public class FibbonaciNo {
     public static void main(String[] args) {
-        System.out.println(fibo(6));
+        System.out.println(fibo(4));
 
     }
     static  int fibo(int n){
---
+++ Recurrsion/src/RevNum.java
@@ -0,0 +1,15 @@
+public class RevNum {
+    public static void main(String[] args) {
+
+    }
+    //first way
+    static int sum=0;
+    static void sum(int n){
+        if(n==0){
+            return;
+        }
+
+
+
+    }
+}
