f76cac9af551379de2524a33a9bec73dc3883737
Muskan-1003 <Muskanpandey1003@gmail.com>
2023-12-11 23:52:42 +0530

    java

---
+++ LinkedList/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+//        LL list=new LL();
+//        list.insertFirst(3);
+//        list.insertFirst(7);
+//        list.insertFirst(6);
+//        list.insertFirst(5);
+//        list.insertFirst(4);
+//        list.insertFirst(2);
+//
+//        list.display();
+
+        DoublyLL list=new DoublyLL();
+        list.insertFirst(3);
+        list.insertFirst(7);
+        list.insertFirst(6);
+        list.insertFirst(5);
+        list.insertFirst(4);
+        list.insertFirst(2);
+
+        list.display();
+
+
+
+
+
+    }
+}
