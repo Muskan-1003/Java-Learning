2d035ce16a2f8deb81e47dbd098eacdd5abe5c0a
Muskan-1003 <Muskanpandey1003@gmail.com>
2024-01-19 23:12:45 +0530

    java

--- BinaryTree/src/Main.java
+++ BinaryTree/src/Main.java
@@ -1,5 +1,12 @@
+
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+       Insert tree = new Insert();
+        tree.populate(scanner);
+        tree.display();
     }
 }
